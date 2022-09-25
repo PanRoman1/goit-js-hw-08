11401ac1859db0bb09decedaa8d036aaca5cc827
PanRoman1 <104255693+PanRoman1@users.noreply.github.com>
2022-09-25 23:45:58 +0300

    Виконане ДЗ8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+addMarkup(galleryRef, galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function makeImageMarkup({ preview, original, description }) {
+  return `
+<div class="gallery__item">
+      <a href="${original}" class="gallery__link">
+        <img
+        src="${preview}"
+        alt="${description}"
+        data-source="${original}"
+        class="gallery__image">
+      </a>
+    </div>
+    `;
+}
+
+function makeGalleryMarkup(images) {
+  return images.map(image => makeImageMarkup(image)).join('');
+}
+
+function addMarkup(placeRefForMarkup, galleryItems) {
+  placeRefForMarkup.insertAdjacentHTML(
+    'beforeend',
+    makeGalleryMarkup(galleryItems)
+  );
+}
